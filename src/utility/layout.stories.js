8c0c64b51171fef81a217e6fb61db15a354ddcb4
Matt Styles <matt@personalurban.com>
2019-06-06 08:50:43 +0100

    add scroll observable

--- src/utility/layout.stories.js
+++ src/utility/layout.stories.js
@@ -1,9 +1,10 @@
 
 import { storiesOf } from '@storybook/react'
-import oc from 'open-color'
+// import oc from 'open-color'
 import styled from 'styled-components'
 
-import { View, Pane, H1, H2, P, Scrollable, TextBlock } from '../'
+import { View, Pane, H1, H2, P, TextBlock,
+  Scrollable, ScrollTarget } from '../'
 
 const Image = styled('img')`
   width: 100%;
@@ -50,6 +51,9 @@ storiesOf('Layout', module)
         <P>Great turbulent clouds venture the ash of stellar alchemy citizens of distant epochs a still more glorious dawn awaits shores of the cosmic ocean. Another world permanence of the stars how far away invent the universe realm of the galaxies kindling the energy hidden in matter.</P>
         <P>From which we spring muse about courage of our questions rich in mystery the carbon in our apple pies the sky calls to us and billions upon billions upon billions upon billions upon billions upon billions upon billions.</P>
         <Image src='https://picsum.photos/id/1002/400/250' />
+        // <ScrollTarget>
+        //   <div>Scroll Target</div>
+        // </ScrollTarget>
         <H2 mt={4}>Diamonds and alcohol</H2>
         <P>Rings of Uranus trillion quasar galaxies another world dispassionate extraterrestrial observer. Courage of our questions take root and flourish finite but unbounded star stuff harvesting star light astonishment permanence of the stars. Extraordinary claims require extraordinary evidence made in the interiors of collapsing stars the ash of stellar alchemy encyclopaedia galactica concept of the number one invent the universe.</P>
         <P>Invent the universe descended from astronomers made in the interiors of collapsing stars shores of the cosmic ocean rich in mystery kindling the energy hidden in matter and billions upon billions upon billions upon billions upon billions upon billions upon billions.</P>
--- src/utility/scrollable.js
+++ src/utility/scrollable.js
@@ -4,40 +4,122 @@
  * viewport. Children can then determine their visibility and react how they wish.
  */
 
-import React, { useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useContext, createContext } from 'react'
 import styled from 'styled-components'
 import { scroll } from 'raid-streams/screen'
 
 import { View } from './'
 
-export const Scrollable = props => {
-  const ref = useRef(null)
-  useEffect(() => {
-    const scrollable = scroll({
-      el: ref.current // @TODO should be this element
-    })
+export const ScrollContext = createContext()
+export const ScrollConsumer = ScrollContext.Consumer
 
-    const s = scrollable
-      .map(event => {
-        event.payload.bottom = event.payload.top + ref.current.offsetHeight // el size
-        event.payload.right = event.payload.left + ref.current.offsetWidth
-        return event
-      })
-      .observe(
-        event => console.log(event),
-        err => console.error(err)
-      )
+export const ScrollTarget = props => {
+  const value = useContext(ScrollContext)
 
-    return () => {
-      console.log(s)
-      console.log(scrollable)
-      // @TODO how to delete the observable and clean up?
-    }
-  })
+  console.log('target', value)
+
+  if (!value) {
+    return <div>nothing</div>
+  }
 
   return (
-    <View {...props} ref={ref}>
+    <>
+      <pre>{JSON.stringify(value)}</pre>
       {props.children}
-    </View>
+    </>
+  )
+}
+
+const useScrollObservable = ref => {
+  const [value, setValue] = useState(null)
+
+  useEffect(() => {
+    console.log('creating stream', ref)
+    const obs = scroll({ el: ref.current })
+      .map(event => {
+        event.payload.bottom = event.payload.top + ref.current.offsetHeight
+        event.payload.right = event.payload.left + ref.current.offsetWidth
+        event.payload.rect = [
+          event.payload.left,
+          event.payload.top,
+          event.payload.right,
+          event.payload.bottom
+        ]
+        // return event
+        return event.payload.rect
+      })
+
+    const subscription = obs.subscribe({
+      next: setValue,
+      complete: () => console.log('Stream complete'),
+      err: err => console.error(err)
+    })
+
+    return () => {
+      console.log('disposing', subscription)
+      // @TODO clean up
+      subscription.unsubscribe()
+    }
+  }, [ref])
+
+  return [value]
+}
+
+export const Scrollable = props => {
+  const ref = useRef(null)
+
+  const [value] = useScrollObservable(ref)
+
+  // const [vp, setVp] = useState({
+  //   left: 0,
+  //   top: 0,
+  //   right: null,
+  //   bottom: null
+  // })
+
+  // useEffect(() => {
+  //   const stream = scroll({
+  //     el: ref.current // @TODO should be this element
+  //   })
+  //     .map(event => {
+  //       // @TODO mutate or clone?
+  //       event.payload.bottom = event.payload.top + ref.current.offsetHeight
+  //       event.payload.right = event.payload.left + ref.current.offsetWidth
+  //       event.payload.rect = [
+  //         event.payload.left,
+  //         event.payload.top,
+  //         event.payload.right,
+  //         event.payload.bottom
+  //       ]
+  //       return event
+  //     })
+  //     .observe(
+  //       event => {
+  //         const { left, top, right, bottom } = event.payload
+  //         setVp({ left, top, right, bottom })
+  //       },
+  //       err => console.error(err)
+  //     )
+  //
+  //   // setVp({
+  //   //   left: 0,
+  //   //   top: 0,
+  //   //   right: ref.current.offsetWidth,
+  //   //   bottom: ref.current.offsetHeight
+  //   // })
+  //
+  //   return () => {
+  //     console.log('cleanup')
+  //     console.log(stream)
+  //     // @TODO how to delete the observable and clean up?
+  //   }
+  // })
+
+  return (
+    <ScrollContext.Provider value={value}>
+      <View {...props} ref={ref}>
+        {props.children}
+      </View>
+    </ScrollContext.Provider>
   )
 }
