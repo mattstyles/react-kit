23f21505730b737457974ac149d29e306f6cb27a
Matt Styles <matt@personalurban.com>
2019-08-12 17:40:55 +0100

    remove open color from range stories

--- src/controls/range.stories.js
+++ src/controls/range.stories.js
@@ -1,10 +1,9 @@
 
 import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
-import oc from 'open-color'
 import styled from 'styled-components'
 
-import { Range, View, Box, Text, utils } from '../'
+import { Range, View, Box, Text, utils, theme } from '../'
 
 const { getRangeTheme } = utils
 
@@ -19,8 +18,8 @@ const Value = styled('span')`
   line-height: ${props => props.height || 16}px;
   width: 40px;
   text-align: center;
-  background: ${oc.gray[7]};
-  color: ${oc.white};
+  background: ${theme.palette.background[8]};
+  color: ${theme.palette.white};
   font-weight: 600;
   letter-spacing: 0.2px;
 `
@@ -47,8 +46,8 @@ const ValueSlider = props => {
     <Box m={2} display='flex' border='solid 1px black'>
       <Value height={height}>{value.toFixed(2)}</Value>
       <Range
-        background={oc.gray[7]}
-        color={oc.green[4]}
+        background={theme.palette.background[8]}
+        color={theme.palette.primary[4]}
         onChange={setValue}
         min={min}
         max={max}
@@ -84,17 +83,17 @@ storiesOf('Range', module)
   ))
   .add('Simple', () => (
     <View>
-      <Box bg={oc.gray[8]} p='2' width={120}>
+      <Box bg={theme.palette.gray[9]} p='2' width={120}>
         <Range
-          background={oc.gray[7]}
-          color={oc.orange[6]}
+          background={theme.palette.gray[8]}
+          color={theme.palette.primary[5]}
           width={120}
         />
       </Box>
-      <Box bg={oc.gray[8]} py={2} mt={1} width={1}>
+      <Box bg={theme.palette.gray[9]} py={2} mt={1} width={1}>
         <Range
-          background={oc.gray[7]}
-          color={oc.orange[6]}
+          background={theme.palette.gray[8]}
+          color={theme.palette.primary[5]}
           width={1}
         />
       </Box>
@@ -102,22 +101,39 @@ storiesOf('Range', module)
   ))
   .add('Sizes', () => (
     <View>
-      <Box p={1}>
-        <p>Full width</p>
-        <Range background={oc.gray[7]} color={oc.violet[3]} width={1} />
-      </Box>
-      <Box p={1}>
-        <p>Half width</p>
-        <Range background={oc.gray[7]} color={oc.violet[4]} width={0.5} />
-      </Box>
-      <Box p={1}>
-        <p>Set Width (150px)</p>
-        <Range background={oc.gray[7]} color={oc.violet[5]} width={150} />
-      </Box>
-      <Box p={1}>
-        <p>Set Height (24px)</p>
-        <Range background={oc.gray[7]} color={oc.violet[6]} width={150} height={24} />
-      </Box>
+      <Surround>
+        <Text block>Full width</Text>
+        <Range
+          background={theme.palette.gray[8]}
+          color={theme.palette.violet[5]}
+          width={1}
+        />
+      </Surround>
+      <Surround>
+        <Text block>Half width</Text>
+        <Range
+          background={theme.palette.gray[8]}
+          color={theme.palette.violet[5]}
+          width={0.5}
+        />
+      </Surround>
+      <Surround>
+        <Text block>Set Width (150px)</Text>
+        <Range
+          background={theme.palette.gray[8]}
+          color={theme.palette.violet[5]}
+          width={150}
+        />
+      </Surround>
+      <Surround>
+        <Text block>Set Height (24px)</Text>
+        <Range
+          background={theme.palette.gray[8]}
+          color={theme.palette.violet[5]}
+          width={150}
+          height={24}
+        />
+      </Surround>
     </View>
   ))
   .add('Values', () => (
@@ -130,21 +146,23 @@ storiesOf('Range', module)
   ))
   .add('Misc', () => (
     <View>
-      <p>Dynamically setting the color value</p>
-      <Box p={2} mb={3} bg={oc.gray[7]}>
-        <ColorRange background={oc.gray[8]} />
+      <Text block>Dynamically setting the color value</Text>
+      <Box p={2} mb={3} bg={theme.palette.gray[9]}>
+        <ColorRange background={theme.palette.gray[8]} />
       </Box>
-      <p>Indented</p>
-      <Box pl={5} bg={oc.gray[6]}>
-        <Box p={3} pl={5} mb={2} bg={oc.gray[7]}>
-          <ColorRange background={oc.gray[8]} />
+      <Text block>Indented</Text>
+      <Box pl={5} bg={theme.palette.gray[7]}>
+        <Box p={3} pl={5} mb={2} bg={theme.palette.gray[9]}>
+          <ColorRange background={theme.palette.gray[8]} />
         </Box>
       </Box>
-      <p>Linear Gradient</p>
-      <Box p={2} mb={3} bg={oc.gray[7]}>
-        <Range color={`linear-gradient(30deg, ${oc.orange[3]}, ${oc.red[7]} 75%)`} />
+      <Text block>Linear Gradient</Text>
+      <Box p={2} mb={3} bg={theme.palette.gray[9]}>
+        <Range
+          color={theme.gradient.sunset}
+        />
       </Box>
-      <p>Discrete</p>
+      <Text block>Discrete</Text>
       <ValueSlider isDiscrete min={0} max={20} initialValue={5} />
     </View>
   ))
