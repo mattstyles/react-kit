56babc0dafa7877f12804d3f6d4c1e3f909f0529
Matt Styles <matt@personalurban.com>
2020-01-17 10:29:55 +0000

    add dark background to see components in other ways

--- packages/components/.storybook/preview.js
+++ packages/components/.storybook/preview.js
@@ -3,7 +3,8 @@ import { addParameters } from '@storybook/react'
 
 addParameters({
   backgrounds: [
-    { name: 'outrun', value: 'hsl(329,100%,54%)' }
+    { name: 'outrun', value: 'hsl(329, 100%, 54%)' },
+    { name: 'dark', value: 'hsl(267, 39%, 15%)' }
   ],
   // Grid does not honour zoom, so its always 8 px.
   grid: {
