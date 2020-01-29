ee7989c786272c279e43a29cb4af59cdcab045f8
Matt Styles <matt@personalurban.com>
2020-01-29 08:10:28 +0000

    update icons

--- packages/react-basic-kit/.storybook/main.js
+++ packages/react-basic-kit/.storybook/main.js
@@ -23,7 +23,7 @@ const pickAddons = compose(
 module.exports = {
   stories: [
     // `../src/**/*.stories.(${useMDX ? 'mdx' : 'js'})`
-    '../src/type/test.stories.js'
+    '../src/icons/*.stories.js'
   ],
   addons: pickAddons(addons)
 }
--- packages/react-basic-kit/src/icons/icon.js
+++ packages/react-basic-kit/src/icons/icon.js
@@ -1,37 +1,58 @@
 
 import React from 'react'
 import propTypes from 'prop-types'
+import { styled } from 'react-kit-core'
+import { css } from '@styled-system/css'
 
 import { getIcon } from './icons'
 import { Box } from '../layout/box'
 import { tokens } from '../theme/index'
 import { getTransition } from '../theme/utils'
 
-const StyledIcon = React.forwardRef(
-  ({ size, width, height, color, hover, rotation, ...props }, ref) => (
-    <Box
-      ref={ref}
-      {...props}
-      __css={{
-        svg: {
-          width: size || width,
-          height: size || height,
-          fill: color,
-          transition: getTransition('fill', 'transition.main')
-        },
-        ':hover svg': {
-          fill: hover
-        },
-        ...rotation && {
-          transformOrigin: 'center center',
-          transform: `rotate(${rotation}deg)`
-        },
-        display: 'inline-flex',
-        position: 'relative',
-        ...props.__css
-      }}
-    />
-  )
+// const StyledIcon = React.forwardRef(
+//   ({ size, width, height, color, hover, rotation, ...props }, ref) => (
+//     <Box
+//       ref={ref}
+//       {...props}
+//       __css={{
+//         svg: {
+//           width: size || width,
+//           height: size || height,
+//           fill: color,
+//           transition: getTransition('fill', 'transition.main')
+//         },
+//         ':hover svg': {
+//           fill: hover
+//         },
+//         ...rotation && {
+//           transformOrigin: 'center center',
+//           transform: `rotate(${rotation}deg)`
+//         },
+//         display: 'inline-flex',
+//         position: 'relative',
+//         ...props.__css
+//       }}
+//     />
+//   )
+// )
+export const StyledIcon = styled('i')(
+  ({ size, width, height, color, hover, rotation }) => css({
+    svg: {
+      width: size || width,
+      height: size || height,
+      fill: color,
+      transition: getTransition('fill', 'main')
+    },
+    ':hover svg': {
+      fill: hover
+    },
+    ...rotation && {
+      transformOrigin: 'center center',
+      transform: `rotate(${rotation}deg)`
+    },
+    display: 'inline-flex',
+    position: 'relative'
+  })
 )
 StyledIcon.defaultProps = {
   as: 'i'
