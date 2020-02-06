aded10c9be88180dfe5fc80361d3f46a199ffc5d
Matt Styles <matt@personalurban.com>
2020-02-06 10:08:30 +0000

    remove unnecessary styled function and test

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -3,7 +3,3 @@ import * as functions from './functions'
 
 export const sf = functions
 export const sx = sf.sx
-
-// this is unnecessary and a huge waste of time.
-// its not perfect interop with SC or emotion, and the problem it attempts to solve isn't actually a problem as prod mode dedupes styles so that whilst the sx function will get evaluated it happens in the correct order and does not cause issue with excessive styling due to the dedupe of declarations.
-// export { styled } from './styled'
