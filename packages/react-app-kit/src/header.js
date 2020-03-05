aa6cc30caeccd28b4ada7f1c3107728c9c6e57de
Matt Styles <matt@personalurban.com>
2020-03-05 07:50:19 +0000

    add header examples

--- packages/react-app-kit/src/header.js
+++ packages/react-app-kit/src/header.js
@@ -1,20 +1,29 @@
 
 import styled from 'styled-components'
+import propTypes from 'prop-types'
 import { css } from '@styled-system/css'
 import { themeGet } from '@styled-system/theme-get'
-import { Flex, sx } from 'react-basic-kit'
+import { Flex, withSx } from 'react-basic-kit'
 
-export const Header = styled(Flex)(
+export const Header = withSx(styled(Flex))(
   props => css({
+    position: props.fixed ? 'fixed' : 'relative',
     justifyContent: 'space-between',
     alignItems: 'center',
     px: themeGet('tokens.layout.padding')(props),
     py: themeGet('tokens.layout.padding')(props) - 1,
-    boxShadow: 1,
-    flexDirection: 'row'
-  }),
-  sx
+    boxShadow: props.hasBorder && 1,
+    flexDirection: 'row',
+    zIndex: 1
+  })
 )
+Header.propTypes = {
+  ...Flex.propTypes,
+  fixed: propTypes.bool,
+  hasBorder: propTypes.bool
+}
 Header.defaultProps = {
-  as: 'header'
+  as: 'header',
+  fixed: false,
+  hasBorder: true
 }
--- packages/react-app-kit/src/header.stories.js
+++ packages/react-app-kit/src/header.stories.js
@@ -1,6 +1,9 @@
 
-import { View, Button, Box, Text, Flex, Input, ButtonGroup, H2 } from 'react-basic-kit'
-import { FaCarrot, FaCog, FaGithub } from 'react-icons/fa'
+import {
+  withKnobs, text, boolean
+} from '@storybook/addon-knobs'
+import { View, Button, Box, Text, Spread, Input, ButtonGroup, H2 } from 'react-basic-kit'
+import { FaCarrot, FaCog, FaGithub, FaBars } from 'react-icons/fa'
 
 import { addBase } from '../.storybook/base'
 
@@ -9,17 +12,24 @@ import { Header } from './header'
 export default {
   title: 'Header',
   decorators: [
-    addBase()
+    addBase(),
+    withKnobs
   ]
 }
 
 export const Basic = () => {
   return (
     <View>
-      <Header>
-        <Text as='h1'>Title</Text>
+      <Header
+        hasBorder={boolean('hasBorder', true)}
+        sx={{
+          bg: text('Background', 'transparent'),
+          color: text('Color', 'inherit')
+        }}
+      >
+        <Text as='h1'>{text('Title', 'Title')}</Text>
         <Box sx={{ mx: 'auto' }} />
-        <Button variant='transparent'>Nav</Button>
+        <Button variant='transparent' sx={{ color: 'inherit' }}>Nav</Button>
       </Header>
     </View>
   )
@@ -29,13 +39,12 @@ export const Controls = () => {
   return (
     <View>
       <Header sx={{ px: 0 }}>
-        <Flex flexDirection='row' justifyContent='space-between' sx={{ maxWidth: 960, mx: 'auto', flex: 1 }}>
-          <Flex flexDirection='row' alignItems='center'>
+        <Spread sx={{ maxWidth: 960, mx: 'auto', flex: 1 }}>
+          <Spread>
             <FaCarrot size={18} />
             <Text sx={{ ml: 2 }} size={5} as='h1'>Title</Text>
-
-          </Flex>
-          <Flex flexDirection='row' alignItems='center'>
+          </Spread>
+          <Spread>
             <Button colour='blue' size='medium' sx={{ mr: 2, maxHeight: 38 }}>Action</Button>
             <Input
               placeholder='Search...'
@@ -49,12 +58,29 @@ export const Controls = () => {
               <Button as='a' icon tight size='medium' variant='transparent'><FaGithub size={18} /></Button>
               <Button icon tight size='medium' variant='transparent'><FaCog size={18} /></Button>
             </ButtonGroup>
-          </Flex>
-        </Flex>
+          </Spread>
+        </Spread>
       </Header>
-      <View isPadded as='main' sx={{ mx: 'auto', maxWidth: 960 }}>
+      <View isPadded isFlex as='main' sx={{ mx: 'auto', maxWidth: 960 }}>
         <H2>Content</H2>
       </View>
+    </View>
+  )
+}
+
+export const Small = () => {
+  return (
+    <View>
+      <Header hasBorder={false}>
+        <Spread sx={{ flex: 1, maxWidth: '480px', mx: 'auto', position: 'relative' }}>
+          <Button icon tight rounding='pill' size='medium' variant='transparent'><FaBars size={18} /></Button>
+          <Text sx={{ position: 'absolute', width: 'full', textAlign: 'center' }} size={5} as='h1'>Title</Text>
+          <Spread>
+            <Button as='a' href='#' icon tight rounding='pill' size='medium' variant='transparent'><FaGithub size={18} /></Button>
+            <Button icon tight rounding='pill' size='medium' variant='transparent'><FaCog size={18} /></Button>
+          </Spread>
+        </Spread>
+      </Header>
     </View>
   )
 }
