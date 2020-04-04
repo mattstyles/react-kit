9248eb2d7cf5e9a39f6871688759d3208ff19934
Matt Styles <matt@personalurban.com>
2020-04-04 17:54:11 +0100

    remove codeblock from barrel files

--- packages/basic-kit/src/index.js
+++ packages/basic-kit/src/index.js
@@ -36,7 +36,7 @@ export {
 export {
   Text, Badge,
   H1, H2, H3, H4, H5, H6, P, Blockquote,
-  Code, Pre, CodeBlock,
+  Code, Pre,
   List, ListItem, Link
 } from './type/index'
 
--- packages/basic-kit/src/type/index.js
+++ packages/basic-kit/src/type/index.js
@@ -3,7 +3,7 @@ export { Text } from './text'
 
 export {
   H1, H2, H3, H4, H5, H6, P, Blockquote,
-  Code, Pre, CodeBlock,
+  Code, Pre,
   List, ListItem, Link
 } from './type'
 
