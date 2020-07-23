4bee894ad1d7c0a46baa4659142d4c8502d75263
Matt Styles <matt@personalurban.com>
2020-07-23 18:03:18 +0100

    add withMedia to system types

--- packages/@raid-ui/core/system/src/index.js
+++ packages/@raid-ui/core/system/src/index.js
@@ -115,3 +115,16 @@ export {
   size,
   typography
 }
+
+/**
+ * Media queries are bracketed, but otherwise the same type as unbracketed.
+ * This helper adds both variants (bracketed or not)
+ */
+export const withMedia = (...types) => {
+  return propTypes.oneOfType([
+    ...types,
+    propTypes.arrayOf(propTypes.oneOfType([
+      ...types
+    ]))
+  ])
+}
