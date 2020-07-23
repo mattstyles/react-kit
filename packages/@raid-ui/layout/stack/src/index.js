a81a8a929cf5ffde2a925cba4a7f300797883694
Matt Styles <matt@personalurban.com>
2020-07-23 18:22:51 +0100

    add :rocket: stack package

---
+++ packages/@raid-ui/layout/stack/src/index.js
@@ -0,0 +1,97 @@
+
+import styled from 'styled-components'
+import { flexbox } from 'styled-system'
+import systemTypes from '@styled-system/prop-types'
+import { css } from '@styled-system/css'
+import propTypes from 'prop-types'
+
+import { withMedia } from '@raid-ui/system'
+import { sx } from '@raid-ui/core'
+import { Box } from '@raid-ui/container'
+
+/**
+ * On the face of it, Grid is an elegant solution here. We can use stack
+ * horizontally or vertically with very little mucking around and neatly set
+ * a theme-based gap between elements.
+ * However... grids inside grids do not like the justify stuff.
+ * Ordinarily this wouldn't actually be too much of a problem as you could use
+ * the dedicated Grid component (after all, Stack only handles the gaps), but,
+ * using Grid would become a grid inside a grid and will not work.
+ * So we'll stick with this implementation of Stack for now, and do some more
+ * research, and see what comes up.
+ * Expect implementation changes in this area, which may (but hopefully not)
+ * have consequences that would require a major bump.
+ */
+export const Stack = styled(Box)(
+  props => css({
+    display: 'grid',
+    gridGap: props.space
+  }),
+  props => props.row && {
+    gridAutoFlow: 'column'
+  },
+  props => props.tight && {
+    justifyContent: 'start',
+    justifyItems: 'start'
+  },
+  props => {
+    if (props.collapse === false) {
+      return
+    }
+
+    // Rather than getting cute about generating an array of the appropriate
+    // length, we’ll go the long way but use the same array each time this
+    // function returns. This should give the runtime a chance to optimise.
+    if (props.collapse === true || props.collapse <= 1) {
+      return css({
+        gridAutoFlow: ['row', 'column']
+      })
+    }
+
+    if (props.collapse === 2) {
+      return css({
+        gridAutoFlow: ['row', 'row', 'column']
+      })
+    }
+
+    if (props.collapse === 3) {
+      return css({
+        gridAutoFlow: ['row', 'row', 'row', 'column']
+      })
+    }
+
+    if (props.collapse > 3) {
+      return css({
+        gridAutoFlow: ['row', 'row', 'row', 'row', 'column']
+      })
+    }
+
+    // In case a different type is supplied we could drop through
+    return css({
+      gridAutoFlow: ['row', 'column']
+    })
+  },
+  flexbox,
+  sx
+)
+Stack.defaultProps = {
+  space: 2,
+  tight: false,
+  row: false,
+  collapse: false
+}
+Stack.propTypes = {
+  ...Box.propTypes,
+  ...systemTypes.flexbox,
+  row: propTypes.bool,
+  space: withMedia(
+    propTypes.number,
+    propTypes.string
+  ),
+  tight: propTypes.bool,
+  collapse: propTypes.oneOfType([
+    propTypes.number,
+    propTypes.bool
+  ])
+}
+Stack.displayName = 'FiveFootNine'
---
+++ packages/@raid-ui/layout/stack/src/stack.stories.js
@@ -0,0 +1,51 @@
+
+import { LifeBuoy, Music } from 'react-feather'
+import { addBase } from '@internal/storybook'
+import { Box } from '@raid-ui/container'
+import { Spacer } from '@raid-ui/align'
+
+import { Stack } from './index'
+
+export default {
+  title: 'Components/Layout/Stack',
+  decorators: [addBase()]
+}
+
+export const Basic = () => {
+  return (
+    <Stack sx={{ bg: 'background.100', mb: 3 }}>
+      <LifeBuoy size={32} />
+      <Music size={32} />
+    </Stack>
+  )
+}
+
+export const Variants = () => {
+  return (
+    <Box color='white'>
+      <Stack>
+        <Box bg='positive.500'>Standard</Box>
+        <Box bg='positive.600'>Standard</Box>
+        <Box bg='positive.700'>Standard</Box>
+      </Stack>
+      <Spacer py={2} />
+      <Stack tight>
+        <Box bg='info.500'>Tight</Box>
+        <Box bg='info.600'>Tight</Box>
+        <Box bg='info.700'>Tight</Box>
+      </Stack>
+      <Spacer py={2} />
+      <Stack row>
+        <Box bg='critical.500'>Row</Box>
+        <Box bg='critical.600'>Row</Box>
+        <Box bg='critical.700'>Row</Box>
+      </Stack>
+      <Spacer py={2} />
+      <Stack row collapse={2}>
+        <Box bg='warning.500'>Collapse 2</Box>
+        <Box bg='warning.600'>Collapse 2</Box>
+        <Box bg='warning.700'>Collapse 2</Box>
+      </Stack>
+    </Box>
+  )
+}
