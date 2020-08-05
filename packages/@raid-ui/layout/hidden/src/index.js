422bbfb16b0151aa7b5eb5ac470ccfd3124b1ba6
Matt Styles <matt@personalurban.com>
2020-08-05 20:53:33 +0100

    fix visually-hidden import

--- packages/@raid-ui/layout/hidden/src/index.js
+++ packages/@raid-ui/layout/hidden/src/index.js
@@ -3,4 +3,4 @@
  * Reach visually hidden is perfect, no need to muck with it
  * @see https://github.com/reach/reach-ui/blob/main/packages/visually-hidden/src/index.tsx
  */
-export { VisuallyHidden } from '@reach/visuallyHidden'
+export { VisuallyHidden } from '@reach/visually-hidden'
