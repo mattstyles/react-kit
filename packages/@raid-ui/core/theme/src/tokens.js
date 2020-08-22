09f69beb6d4defeb9c94c98054497ba0fd0cb329
Matt Styles <matt@personalurban.com>
2020-08-22 14:12:42 +0100

    add :rocket: ui theme package

---
+++ packages/@raid-ui/core/theme/src/tokens.js
@@ -0,0 +1,28 @@
+
+export const tokens = {
+  type: {
+    // ref: fontSizes
+    baseSize: [3, 2],
+
+    body: {
+      // ref: colors
+      main: 'text.700',
+      inverse: 'text.100',
+      muted: 'text.500'
+    },
+    heading: {
+      // ref: colors
+      main: 'text.800',
+      inverse: 'text.75',
+      muted: 'text.600'
+    }
+  },
+
+  layout: {
+    // ref space/size
+    padding: 4,
+
+    // ref: radii
+    rounding: 3
+  }
+}
