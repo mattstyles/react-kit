d52d73d4e5fa44d624e1092904577ec1bd2352c8
Matt Styles <matt@personalurban.com>
2020-09-12 10:17:20 +0100

    fix visually hidden component export

--- packages/@raid-ui/layout/hidden/src/index.js
+++ packages/@raid-ui/layout/hidden/src/index.js
@@ -3,4 +3,6 @@
  * Reach visually hidden is perfect, no need to muck with it
  * @see https://github.com/reach/reach-ui/blob/main/packages/visually-hidden/src/index.tsx
  */
-export { VisuallyHidden } from '@reach/visually-hidden'
+import Hidden from '@reach/visually-hidden'
+
+export const VisuallyHidden = Hidden
